7bded4047b4e15c074713e516366e20857890f02
FabioRaab <115081438+FabioRaab@users.noreply.github.com>
2022-11-13 18:22:41 +0100

    start and import tweepy

---
+++ notebooks/keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='xKh5xBsoll262uS7PQ0FSQb2F'
+
+consumer_secret='bZedlIapglmZFxSrlKskUfjil6CDBgptMlMpN9KDnOIMnPfnlH'
+
+access_token='2469381001-Yd3ETqxQZ45L6y2YQl4w2morWqjfILNTjkMqdPV'
+
+access_token_secret='pNE8mkY1wd7TPlFXOxPogUXIXUVNs7Hpdn06Oa7bAzPMo'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAC9giQEAAAAAuUvAE4LqdbMnaZ%2F6Pv4qvsrYlT8%3Duh8qskzAn45ODBGolVi1Zas6lyd8JEjUi9NCjyqOXGuW4CrsJt'
